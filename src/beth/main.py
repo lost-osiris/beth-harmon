417c2c2c47f29325962e49823b012706a0fa314f
Matthew Owens <matt@fulcrumlabs.ai>
2023-02-13 23:27:29 -0700

    updated to new discord.py version and logging setup

--- src/beth/main.py
+++ src/beth/main.py
@@ -3,6 +3,10 @@ from discord.ext import commands
 from . import defines
 from . import commands as beth_commands
 
+import logging
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+discord.utils.setup_logging(level=logging.DEBUG, handler=handler, root=False)
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix=defines.COMMAND_PREFIX, intents=intents)
